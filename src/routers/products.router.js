1ba51bf86c8c97f3cae87fd77f5f6e8434782a02
Ricardo Gabriel <riicardogabriel12@gmail.com>
2022-10-12 14:34:35 -0300

    Feito 4ª requisito

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
 
+const validateNewProduct = require('../middlewares/validateNewProduct');
+
 const router = express.Router();
 
 router.get('/', productsController.openProducts);
 router.get('/:id', productsController.openProductsId);
-router.post('/', productsController.createProduct);
+router.post('/', validateNewProduct, productsController.createProduct);
 
 module.exports = router;
